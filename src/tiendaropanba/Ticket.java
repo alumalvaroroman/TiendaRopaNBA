c9862443ca4bcbd4290adff161a5c5b697722600
Alvaro <Alvaro@Pc-05>
2015-04-28 10:22:46 +0200

    Base de datos conectada

---
+++ src/tiendaropanba/Ticket.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendaropanba;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Entity
+@Table(name = "ticket")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ticket.findAll", query = "SELECT t FROM Ticket t"),
+    @NamedQuery(name = "Ticket.findByIdTicket", query = "SELECT t FROM Ticket t WHERE t.idTicket = :idTicket"),
+    @NamedQuery(name = "Ticket.findByNombreEmpresa", query = "SELECT t FROM Ticket t WHERE t.nombreEmpresa = :nombreEmpresa"),
+    @NamedQuery(name = "Ticket.findByNombreVendedor", query = "SELECT t FROM Ticket t WHERE t.nombreVendedor = :nombreVendedor"),
+    @NamedQuery(name = "Ticket.findByFecha", query = "SELECT t FROM Ticket t WHERE t.fecha = :fecha"),
+    @NamedQuery(name = "Ticket.findByHora", query = "SELECT t FROM Ticket t WHERE t.hora = :hora")})
+public class Ticket implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IdTicket")
+    private Integer idTicket;
+    @Column(name = "NombreEmpresa")
+    private String nombreEmpresa;
+    @Column(name = "NombreVendedor")
+    private String nombreVendedor;
+    @Column(name = "Fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "Hora")
+    @Temporal(TemporalType.TIME)
+    private Date hora;
+    @OneToMany(mappedBy = "idTicket")
+    private Collection<Ventas> ventasCollection;
+
+    public Ticket() {
+    }
+
+    public Ticket(Integer idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public Integer getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(Integer idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getNombreVendedor() {
+        return nombreVendedor;
+    }
+
+    public void setNombreVendedor(String nombreVendedor) {
+        this.nombreVendedor = nombreVendedor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    @XmlTransient
+    public Collection<Ventas> getVentasCollection() {
+        return ventasCollection;
+    }
+
+    public void setVentasCollection(Collection<Ventas> ventasCollection) {
+        this.ventasCollection = ventasCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idTicket != null ? idTicket.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ticket)) {
+            return false;
+        }
+        Ticket other = (Ticket) object;
+        if ((this.idTicket == null && other.idTicket != null) || (this.idTicket != null && !this.idTicket.equals(other.idTicket))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tiendaropanba.Ticket[ idTicket=" + idTicket + " ]";
+    }
+    
+}
---
+++ src/tiendaropanba/Ventas.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendaropanba;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Alvaro
+ */
+@Entity
+@Table(name = "ventas")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ventas.findAll", query = "SELECT v FROM Ventas v"),
+    @NamedQuery(name = "Ventas.findByIdVentas", query = "SELECT v FROM Ventas v WHERE v.idVentas = :idVentas"),
+    @NamedQuery(name = "Ventas.findByPrecio", query = "SELECT v FROM Ventas v WHERE v.precio = :precio")})
+public class Ventas implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "IdVentas")
+    private Integer idVentas;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "Precio")
+    private BigDecimal precio;
+    @JoinColumn(name = "Id_Ticket", referencedColumnName = "IdTicket")
+    @ManyToOne
+    private Ticket idTicket;
+    @JoinColumn(name = "Id_Producto", referencedColumnName = "IdProducto")
+    @ManyToOne
+    private Producto_1 idProducto;
+
+    public Ventas() {
+    }
+
+    public Ventas(Integer idVentas) {
+        this.idVentas = idVentas;
+    }
+
+    public Integer getIdVentas() {
+        return idVentas;
+    }
+
+    public void setIdVentas(Integer idVentas) {
+        this.idVentas = idVentas;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Ticket getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(Ticket idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public Producto_1 getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Producto_1 idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idVentas != null ? idVentas.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ventas)) {
+            return false;
+        }
+        Ventas other = (Ventas) object;
+        if ((this.idVentas == null && other.idVentas != null) || (this.idVentas != null && !this.idVentas.equals(other.idVentas))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tiendaropanba.Ventas[ idVentas=" + idVentas + " ]";
+    }
+    
+}
